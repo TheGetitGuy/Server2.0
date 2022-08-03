a79b53fdddff089ce024a5e901035c887f3b4f7e
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-08-03 16:14:08 -0400

    added a get rout for testing

--- routes/email.js
+++ routes/email.js
@@ -8,5 +8,7 @@ router.post('/mailer', function(req, res){
     res.send('sick Email')
     console.log(req.body.Email)
 })
-
+router.get('/mailer', function(req, res){
+    res.send('this should be a post')
+})
 module.exports = router
