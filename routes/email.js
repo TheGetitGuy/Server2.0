57e310addb0bbbffa4613fd22ecddd08ffcd9853
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-08-03 17:15:10 -0400

    big email commit scary

--- routes/email.js
+++ routes/email.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer')
 
 router.use(express.json());
 router.use(express.urlencoded({extended:true}));
 
+const createTransporter = async () => {
+    
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth:{
+            type:"OAuth2",
+            user: process.env.USERVAR,
+            clientId: process.env.OAUTH_CLIENTID,
+            clientSecret:process.env.OAUTH_CLIENT_SECRET,
+            refreshToken: process.env.OAUTH_REFRESH_TOKEN
+            },
+        });
+return transporter
+}
+const sendEmail = async (emailOptions) => {
+    let emailTransporter = await createTransporter();
+    await emailTransporter.sendMail(emailOptions)
+}
+
+
 router.post('/mailer', function(req, res){
     res.send('sick Email')
     console.log(req.body.Email)
+    handleEmail(res.body)
 })
 router.get('/mailer', function(req, res){
     res.send('this should be a post')
 })
 module.exports = router
+
+
+function handleEmail(response){
+    sendEmail({
+        subject:`msg from ${response.Email}`,
+        text:`Subject-${response.Subject}:${response.Body}`,
+        to:'thegetitguy@gmail.com'
+    })
+
+}
