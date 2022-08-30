80b6b8353be2a94a6af658f50f3acbb69c08daa7
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-08-30 15:50:27 -0400

    trying get instead of post

--- routes/email.js
+++ routes/email.js
@@ -10,21 +10,21 @@ router.use(express.urlencoded({extended:true}));
 //const url = 'https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send';
 
 
-router.post('/mailer', function(req, res){
+router.get('/mailer', function(req, res){
     res.send('sick Email')
     console.log(req.body.Email)
     handleEmail(req.body)
 })
-router.get('/mailer', function(req, res){
-    res.send('this should be a post')
-})
+//router.get('/mailer', function(req, res){
+ //   res.send('this should be a post')
+//})
 function sendEmail(resp){
     const url = new URL('http://xdroid.net/api/message')
     const k =(process.env.NOTIFKEY)
     const t = resp.subject
     const c = ("body: "+ resp.text)
     url.searchParams.append("k",k)
-    url.searchParams.append("t",k)
+    url.searchParams.append("t",t)
     url.searchParams.append("c",c)
     fetch(url)
 	.then(res => res.json())
