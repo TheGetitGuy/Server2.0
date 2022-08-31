f8d17a87b0398248a46a7663e88b81ac776562aa
TheGetitGuy <73693333+TheGetitGuy@users.noreply.github.com>
2022-08-30 22:25:15 -0400

    .

--- routes/email.js
+++ routes/email.js
@@ -6,8 +6,7 @@ const env = require('dotenv')
 env.config()
 
 router.use(express.json());
-router.use(express.urlencoded({extended:true}));
-//const url = 'https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send';
+router.use(express.urlencoded({extended:true})); 
 
 
 router.post('/mailer', function(req, res){
@@ -18,7 +17,7 @@ router.get('/mailer', function(req, res){
     res.send('this should be a post')
 })
 function sendEmail(resp){
-    const url = new URL('http://xdroid.net/api/message')
+    const url = new URL('https://xdroid.net/api/message')
     const k =(process.env.NOTIFKEY)
     const t = resp.subject
     const c = ("body: "+ resp.text)
